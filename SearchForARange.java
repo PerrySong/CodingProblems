882551b2ba80449e718d5cba3f8479ddb456675c
spfscmz <psong4@dons.usfca.edu>
2018-06-30 23:54:06 -0700

    Leetcode 34

--- SearchForARange.java
+++ SearchForARange.java
@@ -17,38 +17,40 @@ public class SearchForARange {
      */
 
     public int[] searchRange(int[] nums, int target) {
-        int index = binarySearch(nums, target);
+        if (nums == null || nums.length == 0) return new int[]{-1, -1};
         int[] res = new int[2];
-        res[0] = index;
-        res[1] = index;
-        if(index == -1) return res;
-
-        while(res[1] + 1 <= nums.length - 1 && nums[res[1] + 1] == target)
-            res[1]++;
-        while(res[0] - 1 >= 0 && nums[res[0] - 1] == target)
-            res[0]--;
-
+        res[0] = findFirst(nums, target);
+        res[1] = findLast(nums, target);
         return res;
-
     }
 
-    private int binarySearch(int[] nums, int target) {
-        if(nums == null || nums.length == 0) return -1;
+    private int findFirst(int[] nums, int target) {
         int start = 0, end = nums.length - 1;
-        while(start + 1 < end) {
+        while (start + 1 < end) {
             int mid = start + (end - start) / 2;
-            if(nums[mid] == target) {
-                return mid;
-            }
-            if(target < nums[mid]) {
+            if (target > nums[mid]) {
+                start = mid;
+            } else {
                 end = mid;
             }
-            if(target > nums[mid]) {
+        }
+        if (nums[start] == target) return start;
+        if (nums[end] == target) return end;
+        return -1;
+    }
+
+    private int findLast(int[] nums, int target) {
+        int start = 0, end = nums.length - 1;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (target < nums[mid]) {
+                end = mid;
+            } else {
                 start = mid;
             }
         }
-        if(nums[start] == target) return start;
-        if(nums[end] == target) return end;
+        if (nums[end] == target) return end;
+        if (nums[start] == target) return start;
         return -1;
     }
 }
