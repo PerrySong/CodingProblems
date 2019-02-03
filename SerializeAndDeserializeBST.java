3dfb7627b2058fc1120daecc58b3a7140ffd1756
spfscmz <psong4@dons.usfca.edu>
2019-02-02 23:33:30 -0800

    Daily Practicing

--- SerializeAndDeserializeBST.java
+++ SerializeAndDeserializeBST.java
@@ -2,6 +2,15 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Stack;
 
+/**
+     Serialization is the process of converting a data structure or object into a sequence of bits so that it can be stored in a file or memory buffer, or transmitted across a network connection link to be reconstructed later in the same or another computer environment.
+
+     Design an algorithm to serialize and deserialize a binary search tree. There is no restriction on how your serialization/deserialization algorithm should work. You just need to ensure that a binary search tree can be serialized to a string and this string can be deserialized to the original tree structure.
+
+     The encoded string should be as compact as possible.
+
+     Note: Do not use class member/global/static variables to store states. Your serialize and deserialize algorithms should be stateless.
+ */
 public class SerializeAndDeserializeBST {
     // Encodes a tree to a single string.
     // inorder
@@ -32,6 +41,7 @@ public class SerializeAndDeserializeBST {
 
     // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
+        if (data == null) return null;
         String[] nodes = data.split(",");
         Queue<TreeNode> queue = new LinkedList<>();
         for (String node : nodes) {
