b79bb798871e7159df86147b60be0c77cfb0df42
spfscmz <psong4@dons.usfca.edu>
2019-02-23 00:16:49 -0800

    Daily Practicing

--- CountingWithRules.java
+++ CountingWithRules.java
@@ -3,162 +3,176 @@ import java.util.*;
 
 public class CountingWithRules {
     private static final int ONE_MIN = 60;
-    private static final int FIVE_MINS_REQUEST_LIMIT = 10;
+    private static final int FIVE_MIN_WINDOW_BAN_THREADHOLD = 10;
 
-//    class Client {
-//        int blockUntil;
-//        int curStartTime;
-////        int curFiveMinStart;
-//        int oneMinRequests;
-//        int fiveMinRequests;
-//        public Client() {
-//            blockUntil = 0;
-//            curStartTime = 0;
-////            curFiveMinStart = 0;
-//
-//            oneMinRequests = 0;
-//            fiveMinRequests = 0;
-//        }
-//
-//        public boolean allowRequest(int timeStamp) {
-//            int startTime = (timeStamp / ONE_MIN) * ONE_MIN;
-////            int fiveMinStartTime = ((timeStamp / ONE_MIN) / 5) * ONE_MIN;
-//            if (timeStamp != curStartTime) {
-//                curStartTime = timeStamp;
-//            }
-//
-//            if (curStartTime > blockUntil) {
-//                oneMinRequests++;
-//                fiveMinRequests++;
-//                return true;
-//            }
-//            return false;
-//        }
-//
-//        public void block(int time) {
-//            blockUntil = curStartTime + time;
-//        }
-//
-//
-//    }
+        class Request implements Comparable<Request> {
+            String clientName;
+            int timeStamp;
 
-    class Request implements Comparable<Request> {
-        String clientName;
-        int timeStamp;
+            public Request(String req) {
+                String[] tokens = req.split("\\s+");
+                clientName = tokens[0];
+                timeStamp = Integer.parseInt(tokens[1]);
+            }
 
-        public Request(String req) {
+            public int compareTo(Request req2) {
+                return timeStamp - req2.timeStamp;
+            }
 
-            String[] tokens = req.split("\\s+");
-            clientName = tokens[0];
-            timeStamp = Integer.parseInt(tokens[1]);
+            public String toString() {
+                return clientName + " " + timeStamp;
+            }
         }
 
-        public int compareTo(Request req2) {
-            return timeStamp - req2.timeStamp;
-        }
+        public String[] solution(String[] A, int Y) {
 
-        public String toString() {
-            return clientName + " " + timeStamp;
-        }
-    }
+            if (A == null || A.length == 0) return null;
+            String[] res;
+            Map<String, Integer> nameToRequests = new HashMap<>(); // [name : number of successful requests]
+            Map<String, List<Integer>> nameToLastFiveMinReq = new HashMap<>(); // Records the number of requests for every min in last five min
+            Map<String, Integer> nameToBlockedUntil = new HashMap<>();
+            Set<String> clientNames = new LinkedHashSet<>();
+            List<Request> requests = parseToSortedRequests(A, clientNames);
 
-    public String[] solution(String[] A, int Y) {
+            int curMin = 0;
+            for (Request req : requests) {
 
-        Map<String, Integer> nameToRequest = new HashMap<>();
-        List<Request> requests = sortRequest(A);
-        Map<String, List<Integer>> nameToLastFiveMinReq = new HashMap<>();
-        Map<String, Integer> nameToBannedUntil = new HashMap<>();
-        String[] res;
+                String name = req.clientName;
+                int nextMin = req.timeStamp / ONE_MIN;
+                int lastMinReq;
+                List<Integer> lastFiveMinRequests = nameToLastFiveMinReq.getOrDefault(name, new LinkedList<>());
 
-        int curMin = 0;
-        for (Request req : requests) {
+                // Go into next min
+                if (nextMin > curMin) {
+                    // Update blacklist
+                    updateBlackList(name, curMin, nameToLastFiveMinReq, nameToBlockedUntil);
+                }
 
-            String name = req.clientName;
-            int nextMin = req.timeStamp / ONE_MIN;
-            int reqInOneMin;
+                // update every list to the current timestamp
+                for (int i = 0; i < 5 && curMin < nextMin; i++, curMin++) {
+                    nextMin(nameToLastFiveMinReq);
+                }
+                // update curMin
+                curMin = nextMin;
 
-            List<Integer> lastFiveMinRequest = nameToLastFiveMinReq.getOrDefault(name, new LinkedList<>());
+                lastMinReq = lastFiveMinRequests.isEmpty() ? 0 : lastFiveMinRequests.get(lastFiveMinRequests.size() - 1);
 
-            if (nextMin > curMin) { // if finishing current min, we check previous 5 mins requests
-                // If exceed five min's requests limit, ban two mins
-                int fiveMinQeq = sum(lastFiveMinRequest);
-                int total = totalRequest(nameToLastFiveMinReq);
-                if (fiveMinQeq * 2 > total && total >= 10) {
-                    nameToBannedUntil.put(name, curMin + 2);
+                // Update block list
+                if (lastMinReq >= Y) {
+                    // If exceed one min requests limit, ban next one min
+                    nameToBlockedUntil.put(name, Math.max(nameToBlockedUntil.getOrDefault(name, 0), nextMin + 1));
+                }
+
+                // Process current request
+                if (!nameToBlockedUntil.containsKey(name) || nameToBlockedUntil.get(name) <= nextMin) {
+                    allowOneRequest(name, nameToLastFiveMinReq, nameToRequests, lastFiveMinRequests);
                 }
             }
 
-            // update every list to the current timestamp
-            for (int i = 0; i < 5 && curMin < nextMin; i++, curMin++) {
-                pollPrevFiveMin(nameToLastFiveMinReq);
-            }
-            curMin = nextMin;
-
-            reqInOneMin = lastFiveMinRequest.isEmpty() ? 0 : lastFiveMinRequest.get(lastFiveMinRequest.size() - 1);
-
-            if (reqInOneMin >= Y) {
-                // If exceed one min requests limit, ban next one min
-                nameToBannedUntil.put(name, Math.max(nameToBannedUntil.getOrDefault(name, 0), nextMin + 1));
+            // Form the result
+            res = new String[nameToRequests.size()];
+            int i = 0;
+            for (String name : clientNames) {
+                res[i++] = name + " " + nameToRequests.get(name);
             }
 
-
-            // Accept current request
-            if (!nameToBannedUntil.containsKey(name) || nameToBannedUntil.get(name) <= nextMin) {
-                if (lastFiveMinRequest.isEmpty()) lastFiveMinRequest.add(1);
-                else 
-                    lastFiveMinRequest.set(lastFiveMinRequest.size() - 1, reqInOneMin + 1);
-                // Update the request num
-                nameToRequest.put(name, nameToRequest.getOrDefault(name, 0) + 1);
-                System.out.println(req);
-            }
-            nameToLastFiveMinReq.put(name, lastFiveMinRequest);
+            return res;
         }
 
-        res = new String[nameToRequest.size()];
-        int i = 0;
-        for (String name : nameToRequest.keySet()) {
-            res[i++] = name + " " + nameToRequest.get(name);
-        }
-        return res;
-    }
+    /**
+     * Allow client "name" make one request, update relative data
+     * @param name
+     * @param nameToLastFiveMinReq
+     * @param nameToRequests
+     * @param lastFiveMinRequests
+     */
+        private void allowOneRequest(String name, Map<String, List<Integer>> nameToLastFiveMinReq, Map<String, Integer> nameToRequests, List<Integer> lastFiveMinRequests) {
 
+            if (lastFiveMinRequests.isEmpty()) {
+                lastFiveMinRequests.add(1);
+                nameToLastFiveMinReq.put(name, lastFiveMinRequests);
+            } else {
+                lastFiveMinRequests.set(lastFiveMinRequests.size() - 1, lastFiveMinRequests.get(lastFiveMinRequests.size() - 1) + 1);
+            }
+            // Update the request num
+            nameToRequests.put(name, nameToRequests.getOrDefault(name, 0) + 1);
+        }
+
+    /**
+     * Check if current client should be put into blacklist, and update the blacklist
+     * @param name
+     * @param curMin
+     * @param nameToLastFiveMinReq
+     * @param nameToBlockedUntil
+     */
+        private void updateBlackList(String name, int curMin, Map<String, List<Integer>> nameToLastFiveMinReq, Map<String, Integer> nameToBlockedUntil) {
+            List<Integer> lastFiveMinRequests = nameToLastFiveMinReq.get(name);
+            int fiveMinReq = sum(lastFiveMinRequests);
+            int total = totalRequest(nameToLastFiveMinReq);
+            if (fiveMinReq * 2 > total && total >= FIVE_MIN_WINDOW_BAN_THREADHOLD) {
+                nameToBlockedUntil.put(name, curMin + 2);
+            }
+        }
+
+    /**
+     * Return the total request in the nameToLastFiveMinReq
+     * @param nameToLastFiveMinReq
+     * @return
+     */
     private int totalRequest(Map<String, List<Integer>> nameToLastFiveMinReq) {
-        int res = 0;
-        for (List<Integer> reqList : nameToLastFiveMinReq.values()) {
-            for (int i : reqList) {
+            int res = 0;
+            for (List<Integer> reqList : nameToLastFiveMinReq.values()) {
+                for (int i : reqList) {
+                    res += i;
+                }
+            }
+            return res;
+        }
+
+    /**
+     * Poll the first elem from the LastFiveMinReq, and add 0 to the end
+     * @param nameToLastFiveMinReq
+     */
+    private void nextMin(Map<String, List<Integer>> nameToLastFiveMinReq) {
+            for (List<Integer> curList : nameToLastFiveMinReq.values()) {
+                if (curList.size() == 5) {
+                    curList.remove(0);
+                    curList.add(0);
+                } else {
+                    curList.add(0);
+                }
+            }
+        }
+
+    /**
+     * Parse
+     * @param A
+     * @param clientNames
+     * @return
+     */
+        private List<Request> parseToSortedRequests(String[] A, Set<String> clientNames) {
+            List<Request> requests = new ArrayList<>();
+            for (String req : A) {
+                clientNames.add(req.split("\\s")[0]);
+                requests.add(new Request(req));
+            }
+            Collections.sort(requests);
+            return requests;
+        }
+
+    /**
+     *
+     * @param list
+     * @return
+     */
+    private int sum(List<Integer> list) {
+            int res = 0;
+            for (int i : list) {
                 res += i;
             }
+            return res;
         }
-        return res;
-    }
 
-    private void pollPrevFiveMin(Map<String, List<Integer>> nameToLastFiveMinReq) {
-        for (List<Integer> curList : nameToLastFiveMinReq.values()) {
-            if (curList.size() == 5) {
-                curList.remove(0);
-                curList.add(0);
-            } else {
-                curList.add(0);
-            }
-        }
-    }
-
-    private int sum(List<Integer> list) {
-        int res = 0;
-        for (int i : list) {
-            res += i;
-        }
-        return res;
-    }
-
-    private List<Request> sortRequest(String[] A) {
-        List<Request> requests = new ArrayList<>();
-        for (String req : A) {
-            requests.add(new Request(req));
-        }
-        Collections.sort(requests);
-        return requests;
-    }
 
     public static void main(String[] args) {
         CountingWithRules a = new CountingWithRules();
